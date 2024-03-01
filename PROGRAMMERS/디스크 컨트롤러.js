5625d93c63845d401004113e9685bb2d530fb9a3
김태훈 <twodf78@naver.com>
2024-03-01 15:09:03 +0900

    JavaScript / 4.481500000000002ms

--- PROGRAMMERS/디스크 컨트롤러.js
+++ PROGRAMMERS/디스크 컨트롤러.js
@@ -1,44 +1,38 @@
-function solution(jobs) {
-    var answer = 0;
-    const jobCount = jobs.length;
-    
-    jobs.sort((a,b)=>{
-        if( a[0] === b[0]) return a[1] - b[1];
-        return a[0] - b[0];
-    })
-    
-    let jobsChecked = jobs.map((v)=>[...v, false]);
-    let timeEndOfCurrentJob = jobs[0][0];
-    
-    let currentQueue = jobsChecked.map((v, i)=>{
-        if(v[0]<= timeEndOfCurrentJob) return i;
-        else return -1;
-    }).filter((v)=>v>=0);
+const getMinVal = (list) =>{
+    return Math.min(...list.map((v)=>v[0]));
+}
 ​
+const getCurrentQueue = (list, currentTime)=>{
+    return list
+    .sort((a,b)=>a[1]-b[1])
+    .map((v, i)=>{
+        if(v[0]<= currentTime) return i;
+        else return -1;
+    })
+    .filter((v)=>v>=0);
+}
+​
+function solution(jobs) {
+    const jobCount = jobs.length;
+​
+    let jobsChecked = jobs.map((v)=>[...v, false]);
+    let timeEndOfCurrentJob = getMinVal(jobsChecked);
+    let currentQueue = getCurrentQueue(jobsChecked,timeEndOfCurrentJob);
     let totalWaitTime = 0;
     
     while(currentQueue.length){
         
         const currentIdx = currentQueue.shift();
-        const [startTime, timeToUse, _] = jobsChecked[currentIdx];
+        const [startTime, timeToUse, checked] = jobsChecked[currentIdx];
         jobsChecked[currentIdx][2] = true; 
         timeEndOfCurrentJob = timeEndOfCurrentJob < startTime ? startTime + timeToUse : timeEndOfCurrentJob + timeToUse;
         totalWaitTime += timeEndOfCurrentJob - startTime;
         
-        console.log("totalWaitTime:>>", totalWaitTime);
-        
-        jobsChecked = jobsChecked.filter((v)=>!v[2]).sort((a,b)=>a[1]-b[1]);
-        currentQueue = jobsChecked.map((v, i)=>{
-            if(v[0]<= timeEndOfCurrentJob) return i;
-            else return -1;
-        }).filter((v)=>v>=0);
-        
+        jobsChecked = jobsChecked.filter((v)=>!v[2]);
+        currentQueue = getCurrentQueue(jobsChecked,timeEndOfCurrentJob);
         if(currentQueue.length === 0 && jobsChecked.length > 0){
-            timeEndOfCurrentJob = Math.min(...jobsChecked.map((v)=>v[0]));
-            currentQueue = jobsChecked.map((v, i)=>{
-                if(v[0]<= timeEndOfCurrentJob) return i;
-                else return -1;
-            }).filter((v)=>v>=0);
+            timeEndOfCurrentJob = getMinVal(jobsChecked);
+            currentQueue = getCurrentQueue(jobsChecked,timeEndOfCurrentJob);
         }
     }
     
