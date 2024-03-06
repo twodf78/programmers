2473a3f0dbb51480187891d26ddddb74b18457c4
김태훈 <twodf78@naver.com>
2024-03-06 23:31:42 +0900

    JavaScript / 9.429499999999999ms

--- PROGRAMMERS/연속 펄스 부분 수열의 합.js
+++ PROGRAMMERS/연속 펄스 부분 수열의 합.js
@@ -1,21 +1,21 @@
-}
 function solution(sequence) {
-  var answer = 0;
-  const pSeq = [];
-  const mSeq = [];
+  let answer = 0;
+  let pSeq = 0;
+  let mSeq = 0;
 ​
   for (let i = 0; i < sequence.length; i++) {
     if (i === 0) {
-      pSeq.push(sequence[i]);
-      mSeq.push(-sequence[i]);
+      pSeq = sequence[i];
+      mSeq = -sequence[i];
     } else if (i % 2 === 0) {
-      pSeq.push(Math.max(pSeq[i - 1] + sequence[i], sequence[i]));
-      mSeq.push(Math.max(mSeq[i - 1] - sequence[i], -sequence[i]));
+      pSeq = (Math.max(pSeq + sequence[i], sequence[i]));
+      mSeq = (Math.max(mSeq - sequence[i], -sequence[i]));
     } else {
-      pSeq.push(Math.max(pSeq[i - 1] - sequence[i], -sequence[i]));
-      mSeq.push(Math.max(mSeq[i - 1] + sequence[i], sequence[i]));
+      pSeq = (Math.max(pSeq - sequence[i], -sequence[i]));
+      mSeq = (Math.max(mSeq + sequence[i], sequence[i]));
     }
-    answer = Math.max(answer, pSeq[i], mSeq[i]);
+    answer = Math.max(answer, pSeq, mSeq);
   }
+​
   return answer;
 }
