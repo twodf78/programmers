7ba1761cab21c77e085dfc97d44eb1853311a39d
김태훈 <twodf78@naver.com>
2024-03-10 02:08:26 +0900

    JavaScript / 14.5896ms

--- PROGRAMMERS/인사고과.js
+++ PROGRAMMERS/인사고과.js
@@ -1,31 +1,18 @@
 function solution(scores) {
-    const [shAttitude, shPeer] = scores[0];
-    const shSum = shAttitude + shPeer;
-    scores.sort((a, b)=>(a[0] + a[1]) - (b[0] + b[1]))
+  let answer = 0;
+  const [wanhoAtt, wanhoPeer] = scores[0];
+  scores = scores.sort((a, b) => (a[0] === b[0] ? a[1] - b[1] : b[0] - a[0]));
     
-    let idx = 0;
-    for(idx; idx< scores.length; idx++){
-        if(scores[idx][0] + scores[idx][1] > shSum){
-            break;
-        }
+  let max = 0;
+  for(let i = 0 ; i < scores.length; i++) {
+    const [att, peer] = scores[i];
+    if (peer < max) {
+      if (att === wanhoAtt && peer === wanhoPeer) return -1;
+    } else {
+      max = Math.max(max, peer);
+      if (att + peer > wanhoAtt + wanhoPeer) answer++;
     }
-    scores = scores.slice(idx);
-    
-    for(let i = 0; i< scores.length; i++){
-        if(scores[i][0] > shAttitude && scores[i][1] > shPeer) return -1;
-    }
-    
-    let answer = scores.length + 1;
-    
-    for(let i = 0; i<scores.length; i++){
-        const [att, peer] = scores[i];
-        for(let j = i + 1; j<scores.length; j++){
-            if(att < scores[j][0] && peer < scores[j][1]){
-                answer--;
-                break;
-            }
-        }
-    }    
-​
-    return answer;
+  }
+  return answer + 1;
 }
+​
