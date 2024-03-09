d02cd9ac2a00cb301c9337ad23e39cf9c3dc703f
김태훈 <twodf78@naver.com>
2024-03-10 01:36:50 +0900

    JavaScript / 237.054ms

--- PROGRAMMERS/인사고과.js
+++ PROGRAMMERS/인사고과.js
@@ -3,16 +3,20 @@ function solution(scores) {
     const [shAttitude, shPeer] = scores[0];
     const shSum = shAttitude + shPeer;
     scores.sort((a, b)=>{
-        let sumA = a[0] + a[1] 
-        let sumB = b[0] + b[1]
-        return sumA - sumB;
+        let sumA = a[0] + a[1];
+        let sumB = b[0] + b[1]; 
+        return  sumA - sumB ;
     })
-    while(scores.length){
-        if(scores[0][0] + scores[0][1] > shSum){
+    
+    let idx = 0;
+    for(idx; idx< scores.length; idx++){
+        if(scores[idx][0] + scores[idx][1] > shSum){
             break;
         }
-        scores.shift()
     }
+​
+    scores = scores.slice(idx);
+    
     for(let i = 0; i< scores.length; i++){
         if(scores[i][0] > shAttitude && scores[i][1] > shPeer) return -1;
     }
