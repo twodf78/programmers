f84c0aa6e2e16c07f289c54fe0fd10a871844018
김태훈 <twodf78@naver.com>
2024-03-16 00:48:21 +0900

    JavaScript / 47.59166666666666ms

---
+++ PROGRAMMERS/과일 장수.js
@@ -0,0 +1,8 @@
+function solution(k, m, score) {
+    score.sort((a, b) => b - a);
+    let answer = 0;
+    for(let i = m-1; i< score.length; i+=m){
+        answer+= score[i] *m;
+    }
+    return answer;
+}
