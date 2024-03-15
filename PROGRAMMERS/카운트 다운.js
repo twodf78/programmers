b15f1f0c87baa388f44ed87e8129fe1079200415
김태훈 <twodf78@naver.com>
2024-03-16 04:03:24 +0900

    JavaScript / 0.5148ms

--- PROGRAMMERS/카운트 다운.js
+++ PROGRAMMERS/카운트 다운.js
@@ -1,4 +1,4 @@
-//         }
+​
 const ablePoint = [];
 for(let i = 1; i<= 20; i++){
     ablePoint.push(i);
@@ -7,17 +7,13 @@ for(let i = 1; i<= 20; i++){
 }
 ablePoint.push(50);
 ​
-const ablePointSet = [...new Set(ablePoint)];
-ablePointSet.sort((a,b)=>a-b)
-​
 const pointMap = {};
-for(const point of ablePointSet){
+for(const point of ablePoint){
     if(point === 50 || (1<= point && point<= 20)){
         pointMap[point] = 1;
     }else{
         pointMap[point] = 0;
     }
-​
 }
 ​
 const underSixty = (target) => {
@@ -28,33 +24,26 @@ const underSixty = (target) => {
     return [0, 0];
 }
 function solution(target) {
-    // console.log("ablePointSet:>>", ablePointSet)
-    if(target<= 60) return underSixty(target);
-    else{
-        let minCount = 100000;
-        let maxSAB = 0;
-        let test = 0;
-        for(let i = 0; i<= Math.floor(target / 50); i++){
-            
-            let tempTarget = target - i*50;
-            let tempCount = i;
-            let tempSAB = i;
+    let minCount = 100000;
+    let maxSAB = 0;
+    for(let i = 0; i<= Math.floor(target / 50); i++){
+        let tempTarget = target - i*50;
+        let tempCount = i;
+        let tempSAB = i;
 ​
-            tempCount += Math.floor(tempTarget / 60);
+        tempCount += Math.floor(tempTarget / 60);
 ​
-            const [add, singleAndBool] = underSixty(tempTarget % 60);
-            tempCount += add;
-            tempSAB += singleAndBool;
-            
-            if(minCount > tempCount){
-                minCount = tempCount;
-                maxSAB = tempSAB;
-                test = i;
-            }else if(minCount === tempCount){
-                maxSAB = Math.max(maxSAB, tempSAB);
-            }
+        const [add, singleAndBool] = underSixty(tempTarget % 60);
+        tempCount += add;
+        tempSAB += singleAndBool;
+​
+        if(minCount > tempCount){
+            minCount = tempCount;
+            maxSAB = tempSAB;
+        }else if(minCount === tempCount){
+            maxSAB = Math.max(maxSAB, tempSAB);
         }
-        return [minCount, maxSAB];
     }
+    return [minCount, maxSAB];
 }
 ​
