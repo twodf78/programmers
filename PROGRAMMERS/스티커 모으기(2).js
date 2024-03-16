c8750c9517f83fb8132a6179ed2ff9ee8324fc8e
김태훈 <twodf78@naver.com>
2024-03-17 00:56:10 +0900

    JavaScript / 2.5281081081081087ms

---
+++ PROGRAMMERS/스티커 모으기(2).js
@@ -0,0 +1,14 @@
+function solution(sticker) {
+    if(sticker.length <= 3) return Math.max(...sticker)
+    const dpLen = sticker.length + 2;
+    const front = Array.from({length: dpLen}, _=>0);
+    const back = Array.from({length: dpLen}, _=>0);
+    
+    for(let i = 2; i< dpLen - 1; i++){
+        front[i] = Math.max(front[i-1], front[i - 2] + sticker[i -2]);
+    }
+    for(let i = 3; i< dpLen; i++){
+        back[i] = Math.max(back[i-1], back[i - 2] + sticker[i - 2]);
+    }
+    return Math.max(front[dpLen -2], back[dpLen - 1]);
+}
