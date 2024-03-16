0d53a3d75063ca58c4ede454f30e23a2b8902b69
김태훈 <twodf78@naver.com>
2024-03-16 20:41:39 +0900

    JavaScript / 2310.3091666666664ms

--- PROGRAMMERS/소수 찾기.js
+++ PROGRAMMERS/소수 찾기.js
@@ -1,11 +1,46 @@
-function solution(n) {
-    const list = Array.from({length: n}, _ => 0);
-    const root = Math.sqrt(n) >> 0;
-    list[0] = 1;
-    for(let i = 2; i<= root; i++){
-        for(let j = i + i; j<=n; j+=i){
-            list[j - 1] = 1;
+const makePrime = () =>{
+    let primeNum = Array.from({length: 10000000}, (v, i) => i);
+    primeNum[0] = 0;
+    primeNum[1] = 0;
+    for(let i = 2; i<= Math.sqrt(primeNum.length) >> 0; i++){
+        for(let j = i+i; j<= primeNum.length; j+=i){
+            primeNum[j] = 0;
         }
     }
-    return list.filter((v)=>v===0).length;
+    return primeNum.filter((v)=>v);
 }
+​
+const visit = (n, idx) =>{
+    return Array.from({length: n}, (v, i) => i === idx);
+}
+function solution(numbers) {
+    const primeNum =  makePrime();
+    
+    const numList = numbers.split("");
+    const numSet = new Set();
+    
+    const dfs = (str, visit) =>{
+        numSet.add(Number(str));
+        for(let i = 0; i< numbers.length; i++){
+            if(visit[i]) continue;
+            const newVisit = [...visit];
+            newVisit[i] = true;
+            dfs(str + numbers[i], newVisit);
+        }
+    }
+    
+    for(let i = 0; i< numList.length; i++){
+        dfs(numList[i], visit(numList.length, i));
+    }
+    
+    
+    
+    const list = [...numSet];
+    let answer = 0;
+    for(const num of list){
+        if(primeNum.indexOf(num)>=0) answer++;
+    }
+    return answer;
+}
+​
+​
