26226fb8ca8516e356c6e22214c7e75dc78c1537
김태훈 <twodf78@naver.com>
2024-03-17 16:10:35 +0900

    JavaScript / 1.1123076923076924ms

--- PROGRAMMERS/전력망을 둘로 나누기.js
+++ PROGRAMMERS/전력망을 둘로 나누기.js
@@ -8,33 +8,26 @@ function solution(n, wires) {
 ​
     const distList = Array.from({length: n + 1},  _ => 0);
     const sumList = Array.from({length: n + 1},  _ => 0);
-    const leftList = wireList.map((v)=>v.length);
 ​
     for(let times = 0; times< n; times++){
         for(let i = 1; i<= n; i++){
-            const left = leftList[i];
+            const left = wireList[i].length;
             if(left === 0) continue;
             if(left === 1){
                 distList[i] = Math.max(distList[i], n - sumList[i] - 1);
-                leftList[i] -= 1;
+                wireList[i].shift();
             }else{
                 for(let j = 0; j<wireList[i].length;j++){
                     const connect = wireList[i][j];
-                    if(leftList[connect] === 0){
-                        distList[i] = Math.max(distList[i], n - distList[connect]); 
-                        leftList[i] -= 1;
+                    if(wireList[connect].length === 0){
+                        distList[i] = Math.max(distList[i], n - distList[connect]);
                         sumList[i] += n - distList[connect];
                         wireList[i] = wireList[i].filter((v, idx)=>idx!==j);;
                         j--;
                     }
                 }
-                if(leftList[i] === 1){
-                    distList[i] = Math.max(distList[i], n - sumList[i] - 1);
-                    leftList[i] -= 1;
-                }
             }
         }
-​
     }
 ​
     distList.shift();
