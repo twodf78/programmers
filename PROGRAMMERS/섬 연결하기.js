1a9d18b1e23d38988f56ca869b81140858f5d575
김태훈 <twodf78@naver.com>
2024-03-17 22:00:45 +0900

    JavaScript / 0.2625ms

--- PROGRAMMERS/섬 연결하기.js
+++ PROGRAMMERS/섬 연결하기.js
@@ -1,34 +1,31 @@
-const getParent = (parent, x) => {
-  return parent[x] === x ? x : getParent(parent, parent[x]);
-};
-​
-const unionParent = (parent, a, b) => {
-  const n1 = getParent(parent, a);
-  const n2 = getParent(parent, b);
-  n1 < n2 ? (parent[n2] = n1) : (parent[n1] = n2);
-};
-​
-const findParent = (parent, a, b) => {
-  const n1 = getParent(parent, a);
-  const n2 = getParent(parent, b);
-  return n1 === n2;
-};
-​
-function solution(n, costs) {
-  let answer = 0;
-  const parent = Array.from({ length: n }, (v, i) => i);
-​
-  costs.sort((a, b) => a[2] - b[2]);
-​
-  for (const cost of costs) {
-    if (!findParent(parent, cost[0], cost[1])) {
-      answer += cost[2];
-      unionParent(parent, cost[0], cost[1]);
+const findParent = (i, parent) =>{
+    return parent[i] === i ? parent[i] : findParent(parent[i], parent);
+}
+const unionParent = (a, b, parent)=>{
+    let aP = findParent(a, parent);
+    let bP = findParent(b, parent);
+    if(aP <= bP){
+        parent[bP] = aP;
+    }else{
+        parent[aP] = bP;
     }
-  }
+}
 ​
-  return answer;
+const isSameParent = (a, b, parent)=>{
+    return findParent(a, parent) === findParent(b, parent);
+}
+function solution(n, costs) {
+    const parent = Array.from({length: n + 1}, (_, i)=>i);
+    let res = 0;
+    costs.sort((a,b)=>a[2]-b[2]);
+    for(const [a, b, cost] of costs){
+        if(isSameParent(a, b, parent))continue;
+        unionParent(a, b, parent);
+        res += cost;
+    }
+    return res;
 }
 ​
 ​
 ​
+    if (!findParent(parent, cost[0], cost[1])) {
