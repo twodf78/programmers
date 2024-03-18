fbbdcae96f618991c672def212a28cdd5e72283c
김태훈 <twodf78@naver.com>
2024-03-18 19:23:45 +0900

    JavaScript / 8.139499999999998ms

--- PROGRAMMERS/디스크 컨트롤러.js
+++ PROGRAMMERS/디스크 컨트롤러.js
@@ -1,38 +1,45 @@
-const getMinVal = (list) =>{
-    return Math.min(...list.map((v)=>v[0]));
-}
-​
-const getCurrentQueue = (list, currentTime)=>{
-    return list
-    .sort((a,b)=>a[1]-b[1])
-    .map((v, i)=>{
-        if(v[0]<= currentTime) return i;
-        else return -1;
-    })
-    .filter((v)=>v>=0);
-}
 ​
 function solution(jobs) {
-    let jobsChecked = jobs.map((v)=>[...v, false]);
-    let timeEndOfCurrentJob = getMinVal(jobsChecked);
-    let currentQueue = getCurrentQueue(jobsChecked,timeEndOfCurrentJob);
-    let totalWaitTime = 0;
+    const len = jobs.length;
+    const visit = Array.from({length: jobs.length}, _ => false);
+    const copy = jobs.map((v, i)=>[...v, i]);
+    copy.sort((a, b)=>{
+        if(a[0] === b[0]) return a[1] - b[1];
+        return a[0] - b[0]
+    })
     
-    while(currentQueue.length){
-        
-        const currentIdx = currentQueue.shift();
-        const [startTime, timeToUse, checked] = jobsChecked[currentIdx];
-        jobsChecked[currentIdx][2] = true; 
-        timeEndOfCurrentJob = timeEndOfCurrentJob < startTime ? startTime + timeToUse : timeEndOfCurrentJob + timeToUse;
-        totalWaitTime += timeEndOfCurrentJob - startTime;
-        
-        jobsChecked = jobsChecked.filter((v)=>!v[2]);
-        currentQueue = getCurrentQueue(jobsChecked,timeEndOfCurrentJob);
-        if(currentQueue.length === 0 && jobsChecked.length > 0){
-            timeEndOfCurrentJob = getMinVal(jobsChecked);
-            currentQueue = getCurrentQueue(jobsChecked,timeEndOfCurrentJob);
-        }
+    const getReady = (list, time) =>{
+        const res = list.filter((v) => v[0] <= time && !visit[v[2]]);
+        res.sort((a, b)=>a[1] - b[1]);
+        return res;
     }
     
-    return (totalWaitTime / jobs.length) >> 0;
+    let crtTime = copy[0][0];
+    let sum = 0;
+    let ready = [copy[0]];
+    while(ready.length){
+        const [start, time, idx] = ready.shift();
+        visit[idx] = true;
+        sum += (crtTime - start + time);
+        crtTime += time;
+        
+        ready = getReady(copy, crtTime);
+        if(!ready.length){
+            const next = copy.find((v)=>!visit[v[2]]);
+            console.log("next:>>", next);
+            if(!next) break;
+            crtTime = next[0];
+            ready = getReady(copy, crtTime);
+        }
+        
+        console.log("crtTime:>>", crtTime)
+        console.log("start:>>", start)
+        console.log("time:>>", time)
+        console.log("idx:>>", idx)
+        console.log("sum:>>", sum)
+        console.log("ready:>>", ready)
+        console.log("visit:>>", visit)
+    }
+        console.log("visit:>>", visit)
+    return (sum / len) >> 0;
 }
