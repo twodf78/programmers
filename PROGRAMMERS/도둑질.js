0cb27bd373cf67a895e8dba8b002441d1e163f7c
김태훈 <twodf78@naver.com>
2024-03-20 00:04:53 +0900

    JavaScript / 59.30899999999999ms

--- PROGRAMMERS/도둑질.js
+++ PROGRAMMERS/도둑질.js
@@ -1,13 +1,13 @@
 function solution(money) {
-    const dpLen = money.length + 2;
-    const front = Array.from({length: dpLen}, _=>0);
-    const back = Array.from({length: dpLen}, _=>0);
-    
-    for(let i = 2; i< dpLen - 1; i++){
-        front[i] = Math.max(front[i-1], front[i - 2] + money[i -2]);
+    const dpLength = money.length + 2;
+    const front = Array.from({length: dpLength}, _ => 0);
+    const back = Array.from({length: dpLength}, _ => 0);
+    for(let i = 2; i< dpLength - 1; i++){
+        front[i] = front[i - 1] > front[i- 2] + money[i - 2] ? front[i - 1] : front[i- 2] + money[i - 2] ;
     }
-    for(let i = 3; i< dpLen; i++){
-        back[i] = Math.max(back[i-1], back[i - 2] + money[i - 2]);
+    for(let i = 3; i< dpLength; i++){
+        back[i] = back[i - 1] > back[i- 2] + money[i - 2] ? back[i - 1] : back[i- 2] + money[i - 2] ;
     }
-    return Math.max(front[dpLen -2], back[dpLen - 1]);
-}
+    return Math.max(front[dpLength - 2], back[dpLength - 1]);
+}    
+​
