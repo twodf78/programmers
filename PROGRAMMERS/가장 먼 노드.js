80d0c24205616433a63d28d207bd42e5d2e5bef1
김태훈 <twodf78@naver.com>
2024-03-20 23:11:23 +0900

    JavaScript / 17.86ms

--- PROGRAMMERS/가장 먼 노드.js
+++ PROGRAMMERS/가장 먼 노드.js
@@ -1,29 +1,46 @@
-function solution(n, edge) {
-    const edgeMap = {};
-    const visitedMap = {};
-    
-    edge.forEach(([a,b])=>{
-        edgeMap[a] ? edgeMap[a].push(b) : edgeMap[a] = [b];
-        edgeMap[b] ? edgeMap[b].push(a) : edgeMap[b] = [a];
-        visitedMap[a] = false;
-        visitedMap[b] = false;
-    })
-    
-    const bfs = (queue) =>{
-        const currentNodeNum = queue.length;
-        queue.forEach((v)=>{
-            visitedMap[v] = true;
-        })
-        
-        let newQueue = new Set();
-        while(queue.length){
-            const node = queue.shift();
-            edgeMap[node].forEach((v)=>{
-                if(!visitedMap[v]) newQueue.add(v);
-            })
+const getMap = (list) => {
+    const map = {};
+    for(const [e1, e2] of list){
+        if(map[e1]){
+            map[e1].push(e2);
+        }else{
+            map[e1] = [e2];
         }
-        return newQueue.size === 0 ? currentNodeNum : bfs([...newQueue]);
+        
+        if(map[e2]){
+            map[e2].push(e1);
+        }else{
+            map[e2] = [e1];
+        }
     }
-​
-    return bfs([1]);
+    return map;
+}
+function solution(n, edge) {
+    const dist = Array.from({length: n}, _ =>Infinity);
+    const map = getMap(edge);
+    
+    dist[1] = 0;
+    let count = 0;
+    const bfs = (queue) => {
+        count++;
+        const newQueue = [];
+        while(queue.length){
+            const idx = queue.shift();
+            const nextList = map[idx];
+            for(let i = 0; i<nextList.length; i++){
+                const next = nextList[i];
+                if(dist[next] <= 50001) continue;
+                dist[next] = count;
+                newQueue.push(next);
+            }
+        }
+        if(newQueue.length){
+            return bfs(newQueue);
+        }else{
+            return;
+        }
+    }
+    
+    bfs([1]);
+    return dist.filter((v) => v === count - 1).length;
 }
