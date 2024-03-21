c374f7db9635265ec93615bbdf055a73cb448df6
김태훈 <twodf78@naver.com>
2024-03-21 17:38:50 +0900

    JavaScript / 43.702750000000016ms

--- PROGRAMMERS/합승 택시 요금.js
+++ PROGRAMMERS/합승 택시 요금.js
@@ -1,73 +1,27 @@
-        i===start ? visit[i] = 1 : visit[i] = false;
-const visit = (n, start) =>{
-    const visit = {};
-    for(let i = 1; i<= n; i++){
-        i===start ? visit[i] = 1 : visit[i] = false;
-    }
-    visit['max'] = 1;
-    return visit;
-}
-function solution(n, startPoint, a, b, fares) {
-    var answer = 0;
-    
-    const map = {};
-    for(const [start, end, cost] of fares){
-        if (map[start]){
-            map[start].push([end, cost]);
-        }else{
-            map[start]= [[end, cost]];
-        }
-        
-        if (map[end]){
-            map[end].push([start, cost]);
+function solution(n, s, a, b, fares) {
+  const dp = Array.from({ length: n + 1 }, (_) => Array.from({ length: n + 1 }, (_) => Infinity));
+  for (let i = 1; i <= n; i++) {
+    dp[i][i] = 0;
+  }
+  for (const [a, b, cost] of fares) {
+    dp[a][b] = cost;
+    dp[b][a] = cost;
+  }
 ​
-        }else{
-            map[end]= [[start, cost]];
+  for (let k = 1; k <= n; k++) {
+    for (let i = 1; i <= n; i++) {
+      for (let j = 1; j <= n; j++) {
+        if (dp[i][j] > dp[i][k] + dp[k][j]) {
+          dp[i][j] = dp[i][k] + dp[k][j];
         }
+      }
     }
-    
-    let min = 100000 * n;
-    let route = {};
-    const bfs = (queue)=>{
-        const newQueue = [];
-        while(queue.length){
-            const [start, visit, sum] = queue.shift();
+  }
 ​
-            const nextVisit = map[start];
-            for(let i = 0 ; i< nextVisit.length; i++){
-                const [next, cost] = nextVisit[i];
-                if(visit[next]) continue;
-                
-                let tempVisit = {...visit};
-                tempVisit['max'] += 1;
-                tempVisit[next] = tempVisit['max'];
-                if(next === startPoint){
-                    if(min > sum + cost){
-                        min = sum + cost;
-                        route = {...tempVisit};
-                    }
-                    continue;
-                }
-                newQueue.push([next, tempVisit, sum + cost]);
-            }
-        }
-        
-        if(newQueue.length){
-            return bfs(newQueue);
-        }else{
-            return;
-        }
-        
-    }
-    
-    bfs([[a, visit(n, a), 0]]);
-    const aMin = min;
-    const aRoute = route;
-    
-    bfs([[b, visit(n, b), 0]]);
-    const bMin = min;
-    const bRoute = route;
-​
-    
-    return answer;
+  let answer = Infinity;
+  for (let i = 1; i <= n; i++) {
+    const val = dp[s][i] + dp[i][a] + dp[i][b];
+    answer = answer > val ? val : answer;
+  }
+  return answer;
 }
