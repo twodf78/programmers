b45eb928f558b44430a90c330f784304a08f34ba
김태훈 <twodf78@naver.com>
2024-04-01 21:52:48 +0900

    py / 0.031052631578947373ms

--- PROGRAMMERS/둘만의 암호.py
+++ PROGRAMMERS/둘만의 암호.py
@@ -1,9 +1,8 @@
+​
 def solution(s, skip, index):
-    alpha = [chr(code) for code in range(97,123)]
+    alpha = [chr(code) for code in range(97, 123)]
     for c in skip:
-        alpha.remove(c) 
-    answer = ""
-    for c in s:
-        idx = (alpha.index(c) + index) % len(alpha)
-        answer+=alpha[idx]
-    return answer
+        alpha.remove(c)
+    
+    transformed_chars = map(lambda c: alpha[(alpha.index(c) + index) % len(alpha)], s)
+    return ''.join(transformed_chars)
