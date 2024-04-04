195a25b1d60aea3172570953f6eaac4640b3e215
김태훈 <twodf78@naver.com>
2024-04-04 21:30:43 +0900

    py / 52.8525ms

--- PROGRAMMERS/할인 행사.py
+++ PROGRAMMERS/할인 행사.py
@@ -9,15 +9,21 @@ def mapMaker(list):
     return map
 ​
 def solution(want, number, discount):
-    answer=0
-    idx = 0
+    answer, idx = 0, 0
     total = reduce(lambda x, y: x+ y, number)
+    map = mapMaker(discount[idx: idx+total])
     while idx + total <= len(discount):
-        map = mapMaker(discount[idx: idx+total])
         for i in range(len(want)):
             if(map.get(want[i]) != number[i]):
                 answer-=1
                 break
+        map[discount[idx]] = map.get(discount[idx]) - 1
         idx += 1
         answer += 1
+        if(idx + total > len(discount)):
+            break
+        if discount[idx + total - 1] in map:
+            map[discount[idx + total - 1]] = map.get(discount[idx + total - 1]) + 1
+        else :
+            map[discount[idx + total - 1]] = 1
     return answer
